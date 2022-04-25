1fef68e678b86c4b980d7d7c9f96d3d22a6d7c61
Sidiney Cassiano <sidineysilva1@gmail.com>
2022-04-25 18:45:17 -0300

    Inicio da layer Application

---
+++ Application/CountGroup/Create.cs
@@ -0,0 +1,33 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.CountGroup
+{
+    public class Create
+    {
+        public class Command : IRequest
+        {
+            public Guid Guid { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Command>
+        {
+            private readonly ILogger<Command> _logger;
+
+            public Handler(ILogger<Command> logger)
+            {
+                _logger = logger;
+            }
+            public Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+            {
+                _logger.LogInformation("Novo grupo criado: [Guid {0}]", request.Guid);
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
